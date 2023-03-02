1b703325b9301c87feaca8925d59e38a6e74290e
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-02 13:21:56 +0530

    generics in TS

--- src/functions.ts
+++ src/functions.ts
@@ -66,20 +66,39 @@ function addThreeDCoordinate(
 
 console.log(addThreeDCoordinate([1, 2, 3], [4, 5, 6]));
 
-function simpleStringState(
-  initialString: string
-): [() => string, (v: string) => void] {
-  let currentString = initialString;
+function simpleState<T>(initialState: T): [() => T, (v: T) => void] {
+  let currentState = initialState;
   return [
-    () => currentString,
-    (v: string) => {
-      currentString = v;
+    () => currentState,
+    (v: T) => {
+      currentState = v;
     },
   ];
 }
 
-const [getString, setString] = simpleStringState("Hello World");
-setString("Hello World 2");
-console.log(getString());
+const [getState, setState] = simpleState<number | null>(20);
+console.log(getState());
+setState(30);
+console.log(getState());
+
+const [getState2, setState2] = simpleState<string | null>(null);
+console.log(getState2());
+setState2("adarsh");
+console.log(getState2());
+
+function ranker<RankItem>(
+  items: RankItem[],
+  rank: (item: RankItem) => number
+): RankItem[] {
+  return items.sort((a, b) => rank(a) - rank(b));
+}
+
+const items = [
+  { name: "A", rank: 10 },
+  { name: "B", rank: 5 },
+  { name: "C", rank: 15 },
+];
+
+console.log(ranker(items, (item) => item.rank));
 
 export {};
