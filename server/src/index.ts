50b4464e1aa243c06be418c13f1ccd646dbe88ca
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-27 08:04:20 +0530

    refactor: Replace mocked data with actual resolvers and data sources

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,44 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
+// import { addMocksToSchema } from "@graphql-tools/mock";
+// import { makeExecutableSchema } from "@graphql-tools/schema";
 
 import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { TrackAPI } from "./track-api";
 
 // mock data
-const mocks = {
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+// const mocks = {
+//   Track: () => ({
+//     id: () => "track_01",
+//     title: () => "Astro Kitty, Space Explorer",
+//     author: () => {
+//       return {
+//         name: "Grumpy Cat",
+//         photo:
+//           "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+//       };
+//     },
+//     thumbnail: () =>
+//       "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+//     length: () => 1210,
+//     modulesCount: () => 6,
+//   }),
+// };
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: { trackAPI: new TrackAPI({ cache }) },
+      };
+    },
+  });
   console.log(`Apollo Server is running on ${url}`);
 }
 
