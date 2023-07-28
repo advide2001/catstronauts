b72d9c6e74c0ba5f93922d310dadfa1c8ed3c3d5
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-28 17:24:31 +0530

    refactor: Update typeDefs to include additional fields for the Track and Module types

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,6 +4,8 @@ export const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    "Get a single track by its unique id"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +21,12 @@ export const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track"
@@ -28,5 +36,14 @@ export const typeDefs = gql`
     name: String!
     "Author's profile picture url"
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
