e987d2d810f13cc39407999cf757eac8eee2ca97
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-28 17:21:25 +0530

    feat: Add getTrack and getTrackModules methods to TrackAPI

--- server/src/track-api.ts
+++ server/src/track-api.ts
@@ -1,6 +1,6 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 
-import { TrackModel, AuthorModel } from "./models";
+import { TrackModel, AuthorModel, ModuleModel } from "./models";
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
@@ -11,5 +11,13 @@ export class TrackAPI extends RESTDataSource {
 
   getAuthor(authorId: string) {
     return this.get<AuthorModel>(`author/${authorId}`);
+  }
+
+  getTrack(trackId: string) {
+    return this.get<TrackModel>(`track/${trackId}`);
+  }
+
+  getTrackModules(trackId: string) {
+    return this.get<ModuleModel[]>(`track/${trackId}/modules`);
   }
 }
