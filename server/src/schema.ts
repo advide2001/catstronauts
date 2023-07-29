75f1bab164977a6bce9649791fa64fd3480d7fac
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-29 21:36:46 +0530

    refactor: Add Mutation type to GraphQL schema

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -8,6 +8,10 @@ export const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncreamentTrackViewsResponse!
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -45,5 +49,16 @@ export const typeDefs = gql`
     title: String!
     "The Module's length in minutes"
     length: Int
+  }
+
+  type IncreamentTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 `;
