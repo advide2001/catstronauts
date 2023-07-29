54fd3c653736de4d0bc903f7287eac201dd0622c
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-29 21:34:58 +0530

    refactor: Add a Mutation resolver for incrementing track views

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -6,10 +6,33 @@ export const resolvers: Resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
-    track: (parent, { id }, { dataSources }, info) => {
+    track: async (parent, { id }, { dataSources }, info) => {
       return dataSources.trackAPI.getTrack(id);
     },
   },
+
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
